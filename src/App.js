f9cce53a8779760c87bc615bdc7dc0fb780c4ce3
SUBID DAS <itsmesubid@gmail.com>
2022-09-18 22:06:25 +0530

    Create `Header template`

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import Header from "./components/Header.jsx";
+
+function App() {
+  let title = "itsme-Subid";
+  let tabs = ["home", "about", "contact"];
+  let colors = [
+    "#403030",
+    "#00010d",
+    "#122031",
+    "#262621",
+    "#1b4140",
+    "#0d2029",
+    "#122640",
+    "#0d1117",
+    "#262626",
+  ];
+  let activeColor = "#0d1117";
+  return (
+    <div>
+      <Header
+        title={title}
+        tabs={tabs}
+        colors={colors}
+        activeColor={activeColor}
+      />
+    </div>
+  );
+}
+
+export default App;
