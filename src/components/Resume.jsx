b16fd1f2f72d32bcb9e44c679241f4fce7e765d9
SUBID DAS <itsmesubid@gmail.com>
2022-09-27 00:26:36 +0530

    Add `Resume.css`

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,32 +1,31 @@
+import "./Resume.css";
 import React from "react";
 
 function Resume({ data, setStep }) {
-//   let dataCopy = JSON.parse(JSON.stringify(data));
-//   console.log(dataCopy);
-//   let parsedData = Object.keys(dataCopy).map((key) => {
-//     return (
-//       <>
-//         {/* <div className="data-item" key={key}> */}
-//         {key} : {dataCopy[key]}
-//         {/* </div> */}
-//       </>
-//     );
-//   });
-//   console.log(parsedData);
+  let reader = new FileReader();
+  reader.readAsDataURL(data.userImage);
+  reader.onload = () => {
+    document.querySelector(".user-image").src = reader.result;
+  };
+  console.log(data.FName);
   return (
     <div className="resume">
       <h3>Resume</h3>
-      {/* <div className="data" style={{ wordBreak: "break-word" }}>
-        {Object.keys(dataCopy).map((key) => (
-          <div key={key}>
-            {key} : {dataCopy[key]}
+      <div className="resume-wrapper">
+        <div className="header">
+          <img className="user-image" alt="" />
+          <div className="user-details">
+            <h4>
+              {data.FName} {data.LName}
+            </h4>
+            <p>{data.applyFor}</p>
           </div>
-        ))}
-      </div> */}
+        </div>
+      </div>
       <button
         type="button"
         onClick={() => {
-          setStep(6);
+          setStep(7);
         }}
       >
         Previous
