a212e0fafc456f614f13b46218e7d1a49ec425a4
SUBID DAS <itsmesubid@gmail.com>
2022-10-02 20:06:15 +0530

    Update `About.jsx`

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,28 @@ function About() {
         ResumeGenius is a simple, straightforward web app that makes it easy to
         make your very own resume in few minutes. There are no bells and
         whistles, just a simple interface that lets you add your details and
-        generators your resume in fraction of seconds. You can also use our
-        advanced filter to set the color of the web app to suit your mood.
-        ResumeGenius is available for free on the web. ResumeGenius doesn't have
-        any paid version that will gives you access to additional features, nor
-        does it have any in-app purchases.
+        generators your resume in fraction of seconds.
+        <br /> You can also use our advanced filter to set the color of the web
+        app to suit your mood. ResumeGenius is available for free on the web.
+        ResumeGenius doesn't have any paid version that will gives you access to
+        additional features, nor does it have any in-app purchases.
       </p>
+      <div class="features">
+        <h3>
+          <div class="svg"></div>
+          Features Overview:
+        </h3>
+        <li>Modern, intuitive user interface.</li>
+        <li>Easy Access.</li>
+        <li>Secure and Free.</li>
+        <li>No In-App purchases.</li>
+        <li>Available on the web.</li>
+        <li>Advance Filter options.</li>
+        <li>Dark Theme / Night mode.</li>
+        <li>Beautiful Resume.</li>
+        <li>Fast Generation.</li>
+        <li>Printable and also Downloadable Resume.</li>
+      </div>
       <div className="open-source">
         <h3>
           <div className="svg"></div>Open Source:
